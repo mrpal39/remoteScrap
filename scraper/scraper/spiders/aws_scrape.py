09f004e31b4ef13558e6a88e8d2def513d405e1e
rahulpal39 <rahul@thinkdatalabs.com>
2023-01-18 10:53:26 +0530

    update fiels

--- scraper/scraper/spiders/aws_scrape.py
+++ scraper/scraper/spiders/aws_scrape.py
@@ -96,7 +96,7 @@ class QuotesSpider(scrapy.Spider):
 
     what = driver.find_element(by=By.ID, value="twotabsearchtextbox")
 
-    InputFile = 'samsung phone'
+    InputFile = 'redmi'
 
     what.send_keys(InputFile)
 
@@ -126,14 +126,14 @@ class QuotesSpider(scrapy.Spider):
         for quote in all_div_quotes.css('.rush-component'):
             # print(quote)
 
-        #     print(quote)
-        # #     json_object = quote.extract()
+            #     print(quote)
+            # #     json_object = quote.extract()
             url = quote.css('a::attr(href)').extract_first()
             if url is not None:
-                demo=isValidURL(url)
+                demo = isValidURL(url)
                 if demo is False:
-                    book_url =self.base_url + url
-                    # print(book_url) 
+                    book_url = self.base_url + url
+                    # print(book_url)
                     yield scrapy.Request(book_url, callback=self.parse_book)
         # #     # pass
 
@@ -143,66 +143,99 @@ class QuotesSpider(scrapy.Spider):
         JobData = response.css('.centerColAlign')
         # print(JobData)
         dumpTitle = JobData.css('#productTitle::text').extract()
+        prodPrice=JobData.css('#corePriceDisplay_desktop_feature_div')
+
+        # ptb = Information.css("table")
+        # pdivTr = ptb.css('tr')
+        price=prodPrice.css('span::text').extract()
         if len(dumpTitle):
             # print()
-            productName=dumpTitle[0]
-            str1 = productName.replace(' ','')
-            fileName=str1[:20]            
-            name=f"{fileName}.json"
-    
+            productName = dumpTitle[0]
+            str1 = productName.replace(' ', '')
+            fileName = str1[:20]
+            name = f"{fileName}.json"
+
             #     # pass
             #     print(response.url)
-    #########################################productOverview_feature_div
-            desc = JobData.css('#productOverview_feature_div')        
-            tb=desc.css("table")
+    # productOverview_feature_div
+            desc = JobData.css('#productOverview_feature_div')
+            tb = desc.css("table")
             divs = tb.css('tr')
             # print(divs)
-            lDetail=[]
+            lDetail = []
 
             for div in divs:
                 Detail = dict()
 
-                productAbout=div.css("span::text").extract()
-                Detail={
-                    productAbout[0]:productAbout[1]
+                productAbout = div.css("span::text").extract()
+                Detail = {
+                    productAbout[0]: productAbout[1]
                 }
                 lDetail.append(Detail)
                 # print(div.css("span::text").extract())
 
             ###############################productOverview_feature_div##########################################
-            featurebullets = JobData.css('#feature-bullets')     
-            fH=featurebullets.css("h1::text").extract()
-            # print(fH)   
-            uls=featurebullets.css("ul")
-            lAbout=[]
-            
+            featurebullets = JobData.css('#feature-bullets')
+            fH = featurebullets.css("h1::text").extract()
+            # print(fH)
+            uls = featurebullets.css("ul")
+            lAbout = []
+
             for fHdiv in uls:
-                f= fHdiv.css("li")
-                lspan=f.css("span::text").extract()
+                f = fHdiv.css("li")
+                lspan = f.css("span::text").extract()
                 lAbout.append(lspan)
                 # print(f.css("span::text").extract())
 ##################################################productDescription##################################################
-            productDescription = response.css('#productDescription_feature_div')
-            ph1=pDescription =productDescription.css("h2::text").extract()
+            productDescription = response.css(
+                '#productDescription_feature_div')
+            ph1 = pDescription = productDescription.css("h2::text").extract()
             # print(ph1[0])
 
-            pDescription =productDescription.css("span::text").extract()
+            pDescription = productDescription.css("span::text").extract()
             # print(pDescription)
+        ##########################################Product information##################################################
+            Information = response.css('#productDetails_feature_div')
+            pInfoH1 = Information.css("h2::text").extract()
+            # print(
+            #     '############################################################################################')
+            # print(pInfoH1[0])
+            tb = Information.css("table")
+            divTr = tb.css('tr')
+            # print(divs)
+            infoList=[]
+            
+            for div in divTr:
 
+                InformationDetail = dict()
+                
+                # print(div.css('th::text').extract())
+                # print(div.css('td::text').extract())
+                infokey=div.css('th::text').extract()
+                infovalue=div.css('td::text').extract()
+                InformationDetail = {
+                    infokey[0]: infovalue[0]
+                }
+                infoList.append(InformationDetail)
 
-            dictionary={
+            # print(ph1[0])
 
-                'url':response.url,
-                'product Name':productName,
-                'productDetail':lDetail,
-                'about':lAbout,
-                 ph1[0]:pDescription[0],
+        ############################################################################################
+
+            dictionary = {
+
+                'url': response.url,
+                'product Name': productName,
+                'price':price[0],
+                'productDetail': lDetail,
+                'about': lAbout,
+                ph1[0]: pDescription[0],
+                pInfoH1[0]:infoList
 
             }
             # # print(dictionary)
-            # with open(os.path.join('../../../dump/',name), "w") as outfile:   
+            # with open(os.path.join('../../../dump/',name), "w") as outfile:
             #     json.dump(dictionary, outfile,ensure_ascii=False, indent=4)
-
 
             directory = f'./dump/'
             file_path = os.path.join(directory, name)
@@ -212,64 +245,11 @@ class QuotesSpider(scrapy.Spider):
             file = open(file_path, "w")
             json.dump(dictionary, file,ensure_ascii=False, indent=4)
 
-    # #     job_post_Data= JobData.css('span.productTitle::text').extract()
-
-    # #     companyLogo = response.css("div.listing-logo")
-    # #     url = companyLogo.css('img').xpath('@src').extract()
-    # #     post_tag=response.css('span.listing-tag::text').extract()
-    # #     Logo_url=urljoin(url[0], urlparse(url[0]).path)  # 'http://example.com/'
-
-    # #     content = response.css('div.listing-container').extract()
-    # #     aurl = response.css('div.apply_tooltip')
-    # #     apply_url = aurl.css('a::attr(href)').extract()#apply link grap
-
-    # #     company = response.css("div.company-card")
-    # #     companyCountry = company.css('h3::text').extract()
-    # #     company_website = company.css('a::attr(href)').extract()
-    # #     company_name = company.css('a::text').extract()
-    # #     companyDetail=company_website[2]
-
-    # #     if(isValidURL(companyDetail) == True):
-    # #         companyUrl = companyDetail
-    # #         print("Yes")
-    # #     else:
-    # #         companyUrl =''
-
-    # #         print("No")
-
-    # #     # companyUrl = self.base_url + company_website[1]
-    # #     c = len(apply_url[0])
-
-    # #     l_apply_url = 'a:1:{s:3:"url";s:' + str(c) + ':"' + apply_url[0] + '";}'
-
-    # #     item['job_title'] = job_title[0]
-    # #     item['job_description'] = content[0]
-    # #     item['company_logo'] = Logo_url
-    # #     item['company_website'] = companyUrl
-    # #     item['company_name']=company_name[0]
-    # #     item['company_email']='admin@remotejobhunt.com'
-    # #     item['company_url']=companyUrl
-    # #     item['job_country']=companyCountry[-1]
-    # #     item['job_state']=companyCountry[-1]
-    # #     item['job_city']=companyCountry[-1]
-    # #     item['job_address']=companyCountry[-1]
-    # #     item['category']=post_tag[1]
-    # #     item['type']=post_tag[0]
-    # #     item['job_zip_code']='145521'
-    # #     item['company_country']=companyCountry[-1]
-    # #     item['company_state']=companyCountry[-1]
-    # #     item['company_zip_code']='45458'
-    # #     item['company_location']=companyCountry[-1]
-    # #     item['wpjobboard_am_data'] =l_apply_url
-    # #     yield ite
-
-        next=response.css(".s-pagination-strip")
-        apply_url = next.css('a::attr(href)').extract()#apply link grap
+        next = response.css(".s-pagination-strip")
+        apply_url = next.css('a::attr(href)').extract()  # apply link grap
         # if len(apply_url):
 
         #     print(apply_url)
-
-            
 
         # next_page = next.css('span.next a::attr(href)').get()
         # if next_page is not None:
